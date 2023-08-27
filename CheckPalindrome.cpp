557181acb49312d8c4cfb9699524de3a29e30bd8
Hasrat Ali <106477102+ali-hasrat@users.noreply.github.com>
2023-08-27 14:32:28 +0530

    Program to check whether the no. is Palindrome or not.

---
+++ CheckPalindrome.cpp
@@ -0,0 +1,31 @@
+//Program to check whether the no. is Palindrome or not
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int palindrome(int n)
+{
+    int rev=0;
+    int temp=n;
+    while(n!=0)
+    {
+        int ld=n%10;
+        rev=rev*10+ld;
+        n=n/10;
+    }
+    cout<<rev<<endl;
+    if(temp==rev)
+    {
+        cout<<"Yes Palindrome"<<endl;
+    }
+    else
+    cout<<"Not Palindrome";
+}
+int main()
+{
+    int n;
+    cout<<"Enter the no. ";
+    cin>>n;
+    palindrome(n);
+    return 0;
+}
